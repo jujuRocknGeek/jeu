049c3af6198f51bc8509b01ae8d4cd7c400440d4
Julien BELLAIS LAITHIER <jbellaislaithier@gmail.com>
2019-01-14 15:00:28 +0100

    probleme venant du ciblage arm

--- damier.js
+++ damier.js
@@ -3,29 +3,34 @@ function RandomPlace(x){
     let choixLigne = Math.floor(tab.length * Math.random());
     let colonne = tab[choixLigne].cells;
     let choixCellule = Math.floor(colonne.length * Math.random());
-    colonne[choixCellule].innerHTML= `<img src=\'images/${x}.png\'/>`;
-    let position = [choixLigne, choixCellule];
-    return position;
+    colonne[choixCellule].innerHTML= x;
 };
-console.log(RandomPlace(vaisseauIni.spriteXw));
-console.log(RandomPlace(vaisseauIni.spriteTie));
-
+RandomPlace(vaisseauIni.spriteXw);
+RandomPlace(vaisseauIni.spriteTie);
 
 
 //fonction qui place les astéroides aléatoirements
-function AsteroidPlace(){   
+function AsteroidPlace(){
+
 //on initialise la boucle qui défini le nombre d'astroides à placer
    const nbAsteroid = 15;
    let x;
    const tab = document.getElementById('tabJeu').rows;
-   const aT = ["<img src='images/asteroid1.png'/>", "<img src='images/asteroid2.png'/>", "<img src='images/asteroid3.png'/>"];
    for( x=0; x<nbAsteroid; x++){
 //choix aléatoire du placement des astéroides sur le damier.
     let choixLigne = Math.floor(tab.length * Math.random());
     let colonne = tab[choixLigne].cells;
     let choixCellule = Math.floor(colonne.length * Math.random());
+        if(colonne[choixCellule].innerHTML == ""){
     colonne[choixCellule].innerHTML= aT[Math.floor(aT.length * Math.random())];
-   }
+        }
+        else{
+            let choixLigne = Math.floor(tab.length * Math.random());
+            let colonne = tab[choixLigne].cells;
+            let choixCellule = Math.floor(colonne.length * Math.random());
+            colonne[choixCellule].innerHTML= aT[Math.floor(aT.length * Math.random())];
+        }
+    }
 };
 
 //placement aléatoire des armes
@@ -37,7 +42,15 @@ function ArmesPlace(){
     let choixLigne = Math.floor(tab.length * Math.random());
     let colonne = tab[choixLigne].cells;
     let choixCellule = Math.floor(colonne.length * Math.random());
+        if(colonne[choixCellule].innerHTML == ""){
     colonne[choixCellule].innerHTML= armes[i];
+        }
+        else{
+          let choixLigne = Math.floor(tab.length * Math.random());
+            let colonne = tab[choixLigne].cells;
+            let choixCellule = Math.floor(colonne.length * Math.random());
+            colonne[choixCellule].innerHTML= aT[Math.floor(aT.length * Math.random())];;
+        }
     }
 };
 
--- joueurs.js
+++ joueurs.js
@@ -11,38 +11,41 @@ let joueurTie= {
     pv : 100
 };
 
+
+let aT = ["<img src='images/asteroid1.png' class='asteroid'/>", "<img src='images/asteroid2.png' class='asteroid'/>", "<img src='images/asteroid3.png' class='asteroid'/>"];
+
 let vaisseauIni = {
-    spriteXw : "xwing1",
-    spriteTie : "tie1",
-    spriteArmeXw : "<img src='images/evoXwing0.png'/>",
-    spriteArmeTie : "<img src='images/evoTie0.png'/>",
+    spriteXw : "<img src='images/xwing1.png' id='xwing' class='ship'/>",
+    spriteTie : "<img src='images/tie1.png' id='tie' class='ship'/>",
+    spriteArmeXw : "<img src='images/evoXwing0.png' id='arm' class='arm1'/>",
+    spriteArmeTie : "<img src='images/evoTie0.png' id='arm'class='arm1'/>",
     degats : 10
 };
 
 let vaisseau1 = {
-    sprite : "<img src='images/xwing2.png'/>",
-    spriteTie : "<img src='images/tie2.png'/>",
-    spriteArme : "<img src='images/evo1.png'/>",
+    spriteXw : "<img src='images/xwing2.png' id='xwing' class='ship'/>",
+    spriteTie : "<img src='images/tie2.png' id='tie' class='ship'/>",
+    spriteArme : "<img src='images/evo1.png' id='arm' class='arm2'/>",
     degats : 20
 };
 
 let vaisseau2 = {
-    sprite : "<img src='images/xwing3.png'/>",
-    spriteTie : "<img src='images/tie3.png'/>",
-    spriteArme : "<img src='images/evo2.png'/>",
+    spriteXw : "<img src='images/xwing3.png' id='xwing' class='ship'/>",
+    spriteTie : "<img src='images/tie3.png' id='tie' class='ship'/>",
+    spriteArme : "<img src='images/evo2.png' id='arm' class='arm3'/>",
     degats : 25
 };
    
 let vaisseau3 = {
-    sprite : "<img src='images/xwing4.png'/>",
-    spriteTie : "<img src='images/tie4.png'/>",
-    spriteArme : "<img src='images/evo3.png'/>",
+    spriteXw : "<img src='images/xwing4.png' id='xwing'class='ship'/>",
+    spriteTie : "<img src='images/tie4.png' id='tie' class='ship'/>",
+    spriteArme : "<img src='images/evo3.png' id='arm' class='arm4'/>",
     degats : 30
 };
 
 let vaisseau4 = {
-    sprite : "<img src='images/xwing5.png'/>",
-    spriteTie : "<img src='images/tie5.png'/>",
-    spriteArme : "<img src='images/evo4.png'/>",
+    spriteXw : "<img src='images/xwing5.png' id='xwing' class='ship'/>",
+    spriteTie : "<img src='images/tie5.png' id='tie' class='ship'/>",
+    spriteArme : "<img src='images/evo4.png' id='arm'class='arm5'/>",
     degats : 35
 }
